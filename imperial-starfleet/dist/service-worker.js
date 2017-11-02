7515006eecf58c7b17eb35bce604b4a99ece98a5
jpleva91 <jpleva91@gmail.com>
2017-11-02 15:14:05 -0600

    add dist

---
+++ imperial-starfleet/dist/service-worker.js
@@ -0,0 +1,33 @@
+var CACHE_NAME = 'service-worker-cache-v1';
+var urlsToCache = [
+  'index.html',
+  './service-worker.js',
+  'assets/images/tie-fighter.png',
+  './assets/images/turret.png'
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        console.log(response);
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
